e72c557cbde5db20e18eb2bc430b6faea0712d6a
glowdesain <65471059+glowdesain@users.noreply.github.com>
2026-08-29 10:26:26 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
-import{defineConfig}from'vite';import react from'@vitejs/plugin-react';export default defineConfig({plugins:[react()],server:{port:5173,host:true,open:true}})
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+    open: true,
+  },
+})
